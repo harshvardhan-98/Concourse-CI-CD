555ed42ce01a087a8c609b49a9e8882aa5b0d7f6
Harsh Vardhan <harsh.vardhan@knoldus.com>
2022-04-08 15:04:16 +0530

    A

--- src/main/java/com/knoldus/codeinsights/CodeInsightsApplication.java
+++ src/main/java/com/knoldus/codeinsights/CodeInsightsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodeInsightsApplication {
-
+//main
     public static void main(String[] args) {
         SpringApplication.run(CodeInsightsApplication.class, args);
     }
